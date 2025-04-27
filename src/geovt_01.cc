ff68368a1499b735365b7ab26f0a5c6acc7c1eed
Moritz Huber <moritz@Moritzs-MacBook-Pro.local>
2025-04-28 01:08:15 +0200

    geovt_01 changes

--- src/geovt_01.cc
+++ src/geovt_01.cc
@@ -2,7 +2,6 @@
 #include <vector>
 #include <iostream>
 #include <chrono>
-#include <iomanip>
 #include <cassert>
 #include <cstring>
 #include <SDL3/SDL.h>
@@ -15,35 +14,69 @@ using namespace std;
 
 static const int gk_window_width = 1920;
 static const int gk_window_height = 1080;
-static const int gk_objects_max = 1000;
-static const int gk_text_buf_max = 1024;
-static const uint32_t gk_fg_color_default = 0x00000000;
-static const uint32_t gk_bg_color_default = 0xFFFFFFFF;
+static const uint32_t gk_default_fg_color = 0x00000000;
+static const uint32_t gk_default_bg_color = 0xFFFFFFFF;
 
-typedef struct {
-  double x;
-  double y;
-} Point2D;
+static bool line_has_first_point = false;
+static bool circle_has_center = false;
 
-std::vector<Point2D> g_points{}; // empty vector
+// Basic Objects
+struct Point2D {
+	double x {};
+	double y {};
+};
 
-std::vector<int> g_numbers { 5, 8, 9, 1 }; // list constructor is called
-vector<int> g_few_numbers ( 10 ); // explicit constructor to specify length
-vector<int> g_few_numbers2 { 10 }; // matches list constructor, lenght 1
+struct Vec2D {
+	double x {};
+	double y {};
+};
 
-template <typename T>
-T my_max(T x, T y) {
-	return (x < y) ? y : x;
-}
+enum struct ObjectState {
+	HIGHLIGHTED,
+	SELECTED,
+	GHOSTED,
+	NORMAL,
+};
 
-typedef enum {
+// Complex Objects
+struct Line2D {
+	Point2D p1 {};
+	Point2D p2 {};
+	uint32_t color = gk_default_fg_color;
+	vector<Point2D> is_points {};
+	ObjectState state = ObjectState::NORMAL;
+	Line2D(Point2D p1) : p1 {p1} {}
+	Line2D(Point2D p1, Point2D p2, uint32_t color)
+		: p1 {p1}, p2 {p2}, color {color} {}
+};
+
+struct Circle2D {
+ 	Point2D center {};
+	double radius {};
+	uint32_t color = gk_default_fg_color;
+	vector<Point2D> is_points {};
+	ObjectState state = ObjectState::NORMAL;
+	Circle2D(Point2D center) : center {center} {}
+	Circle2D(Point2D center, double radius, uint32_t color)
+		: center {center}, radius {radius}, color {color} {}
+};
+
+// struct Arc
+
+struct Objects {
+	vector<Line2D> lines;
+	vector<Circle2D> circles;
+	vector<Point2D> global_is_points;
+};
+
+// the application
+enum struct AppMode {
   NORMAL,
-  POINT,
   LINE,
 	CIRCLE,
-} AppMode;
+};
 
-typedef struct {
+struct AppState {
 	SDL_Window *window;
 	SDL_Renderer *renderer;
 	SDL_Texture *window_texture;
@@ -56,171 +89,196 @@ typedef struct {
 	Point2D mouse;
   bool mouse_left_down;
   bool mouse_right_down;
-	bool mouse_click;
-
-  // bool text_input;
-  // char text[gk_text_buf_max];
-} AppState;
+};
 
 int app_init(AppState *app);
-void process_event(AppState *app);
-void draw(AppState &app, uint32_t *buffer);
+void process_events(AppState *app);
+void handle_mouse_click(AppState &app, Objects &objects);
+void update(AppState &app, Objects &objects);
+void draw(AppState &app, Objects &objects);
 
-uint32_t *test(AppState &app);
+auto create_line(Objects &objects, Point2D &p0, Point2D &&p1, uint32_t color)
+{
+	objects.lines.push_back(Line2D{ p0, p1, color }); // CHECK: is this valid?
+	cout << "Line Created, " << objects.lines.size() << "lines" << endl;
+	return objects.lines.end();
+}
 
+auto create_line(Objects &objects, Point2D &p0)
+{
+	objects.lines.push_back(Line2D{ p0 }); // CHECK: is this valid?
+	cout << "Line Created, " << objects.lines.size() << "lines" << endl;
+	return objects.lines.end();
+}
 
-// dt no draw:			0.005ms
-// dt with draw:		16.5ms
+auto create_circle(Objects &objects, Point2D &center, double &radius, uint32_t color)
+{
+	objects.circles.push_back(Circle2D{ center, radius, color });
+	cout << "Circle Created, " << objects.circles.size() << "circles" << endl;
+	return objects.circles.end();
+}
+
+auto create_circle(Objects &objects, Point2D &center)
+{
+	objects.circles.push_back(Circle2D{ center});
+	cout << "Circle Created, " << objects.circles.size() << "circles" << endl;
+	return objects.circles.end();
+}
+
 int main() {
-	int counter = 0;
 	AppState app;
+	Objects objects;
 	if (!app_init(&app))
 		return 1;
 
-	uint32_t *buffer = test(app);
 	while(app.keep_running) {
-		// if (counter++ > 100)
-		// 	app.keep_running = 0;
-
-		// std::cout << std::fixed << std::setprecision(12) << dt_ms << std::endl;
-
-		draw(app, buffer);
-		process_event(&app);
-		// SDL_Delay(1);
+		process_events(&app);
+		draw(app, objects);
+		SDL_Delay(10);
 	}
 	return 0;
 }
 
-
-
-int app_init(AppState *app) {
+int app_init(AppState &app) {
   if (!SDL_Init(SDL_INIT_VIDEO)) {
     SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
     return SDL_APP_FAILURE;
   }
 
-	app->window = NULL;
-	app->renderer = NULL;
+	app.window = NULL;
+	app.renderer = NULL;
   if (!SDL_CreateWindowAndRenderer("examples/renderer/streaming-textures",
 				gk_window_width, gk_window_height, SDL_WINDOW_HIGH_PIXEL_DENSITY |
-				SDL_WINDOW_MOUSE_CAPTURE, &app->window, &app->renderer)) {
+				SDL_WINDOW_MOUSE_CAPTURE, &app.window, &app.renderer)) {
     SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
     return SDL_APP_FAILURE;
   }
 
-	int vsync { 3 };
-	assert(SDL_GetRenderVSync(app->renderer, &vsync));
+	app.w_pixels = gk_window_width *
+		SDL_GetWindowPixelDensity(app.window);
+	app.h_pixels = gk_window_height *
+		SDL_GetWindowPixelDensity(app.window);
 
-	std::cout << "vsync: " << vsync << std::endl;
-	// SDL_GetRenderVSync(app->renderer, SDL_RENDERER_VSYNC_DISABLED);
-
-	app->w_pixels = gk_window_width *
-		SDL_GetWindowPixelDensity(app->window);
-	app->h_pixels = gk_window_height *
-		SDL_GetWindowPixelDensity(app->window);
-
-	// texture create with pixels and not window size -> retina display scaling
-  app->window_texture = SDL_CreateTexture(
-			app->renderer, SDL_PIXELFORMAT_XRGB8888,
+	// texture create with pixels and not window size . retina display scaling
+  app.window_texture = SDL_CreateTexture(
+			app.renderer, SDL_PIXELFORMAT_XRGB8888,
 			SDL_TEXTUREACCESS_STREAMING, 
-			app->w_pixels, app->h_pixels);
+			app.w_pixels, app.h_pixels);
 
-	if (!app->window_texture) {
+	if (!app.window_texture) {
     SDL_Log("Couldn't create streaming texture: %s", SDL_GetError());
     return SDL_APP_FAILURE;
 	}
-	app->keep_running = true;
-  app->mouse.x = 0;
-  app->mouse.y = 0;
-  app->mouse_left_down = 0;
-  app->mouse_right_down = 0;
-	app->mouse_click = false;
-  app->density = SDL_GetWindowPixelDensity(app->window);
-	std::cout << "w_pixels: " << app->w_pixels << std::endl;
-	std::cout << "h_pixels: " << app->h_pixels << std::endl;
-  // app->text_input = false;
-  // for (int i = 0; i < TEXT_BUF_MAX; i++) {
-  //   app->text[i] = 0;
-  // }
+	app.keep_running = true;
+  app.mouse.x = 0;
+  app.mouse.y = 0;
+  app.mouse_left_down = 0;
+  app.mouse_right_down = 0;
+  app.density = SDL_GetWindowPixelDensity(app.window);
+	std::cout << "w_pixels: " << app.w_pixels << std::endl;
+	std::cout << "h_pixels: " << app.h_pixels << std::endl;
   return 1;
 }
 
-void process_event(AppState *app) {
+void process_events(AppState &app, Objects &objects) {
   SDL_Event event;
   while (SDL_PollEvent(&event)) {
     switch (event.type) {
     case SDL_EVENT_QUIT:
-      app->keep_running = false;
+      app.keep_running = false;
       break;
 		case SDL_EVENT_KEY_DOWN:
 			switch(event.key.key) {
+        case SDLK_ESCAPE:
+          if (!event.key.repeat) {
+            app.mode = AppMode::NORMAL;
+          }
+          break;
+				case SDLK_C:
+          if (!event.key.repeat) {
+            app.mode = AppMode::CIRCLE;
+          }
+					break;
+        case SDLK_L:
+          if (!event.key.repeat) {
+            app.mode = AppMode::LINE;
+          }
+          break;
+
 				case SDLK_P:
 					if (!event.key.repeat) {
-						cout << "number max: " << my_max<int>(82, 11) << endl;
-						// create point
+						 cout << "circles:" << endl;
+						 for (auto circle : objects.circles) {
+							 cout << "(" << circle.center.x << ", " 
+								 << circle.center.y << ")" << endl;
+						 }
+						 cout << endl;
+						 cout << "lines:" << endl;
+						 for (auto line : objects.lines) {
+							 cout << "(" << line.p1.x << ", " << line.p1.y << ")"
+								 << " | " << "(" << line.p2.x << ", " << line.p2.y << ")"
+								 << endl;
+						 }
+						 cout << endl;
+						 cout << "app mode: ";
+						 switch (app.mode) {
+							 case AppMode::NORMAL: cout << "normal" << endl;
+							 case AppMode::LINE: cout << "line" << endl;
+							 case AppMode::CIRCLE: cout << "cricle" << endl;
+						 }
+						 cout << endl;
 					}
 					break;
 			}
     case SDL_EVENT_MOUSE_MOTION:
-      app->mouse.x = SDL_lround(event.motion.x * app->density);
-      app->mouse.y = SDL_lround(event.motion.y * app->density);
+      app.mouse.x = SDL_lround(event.motion.x * app.density);
+      app.mouse.y = SDL_lround(event.motion.y * app.density);
       break;
     case SDL_EVENT_MOUSE_BUTTON_DOWN:
-      app->mouse_left_down = event.button.down;
-			// handle mouse click event
+      app.mouse_left_down = event.button.down;
+			handle_mouse_click(app, objects);
       break;
     case SDL_EVENT_MOUSE_BUTTON_UP:
-      app->mouse_left_down = event.button.down;
+      app.mouse_left_down = event.button.down;
       break;
 		}
 	}
 }
 
-uint32_t *test(AppState &app) {
-	// uint32_t *buffer = new uint32_t(app.w_pixels * app.h_pixels);
-	uint32_t *buffer = (uint32_t *)malloc(sizeof(uint32_t)
-			* (app.w_pixels * app.h_pixels));
-	for (uint32_t i = 0;	i < app.w_pixels * app.h_pixels; i++) {
-		buffer[i] = 0XFFFFFF00;
-	}	
-	return buffer;
-}
-#include <arm_neon.h> // NEON intrinsics on M1/M2
-
-void fast_fill_neon(uint32_t* dest, uint32_t value, size_t count) {
-    uint32x4_t v = vdupq_n_u32(value); // Duplicate the value into a 128-bit register (4 uint32_t)
-
-    size_t i = 0;
-    for (; i + 4 <= count; i += 4) {
-        vst1q_u32(dest + i, v); // store 4 pixels at once
-    }
-    // fill remaining
-    for (; i < count; ++i) {
-        dest[i] = value;
-    }
+void handle_mouse_click(AppState &app, Objects &objects) {
+	// create shapes
+  switch (app.mode) {
+  case AppMode::NORMAL:
+    break;
+  case AppMode::LINE:
+		if (!line_has_first_point) {
+			create_line(objects, app.mouse);
+			line_has_first_point = true;
+		} else {
+			objects.lines.back().p2 = app.mouse;
+			line_has_first_point = false;
+		}
+    break;
+	case AppMode::CIRCLE:
+		if (!circle_has_center) {
+			create_circle(objects, app.mouse);
+			circle_has_center = true;
+		} else {
+			Circle2D &circle = objects.circles.back();
+			circle.radius = SDL_sqrt(SDL_pow((circle.center.x - app.mouse.x), 2.0) + 
+						SDL_pow((circle.center.x - app.mouse.x), 2.0)); 
+			circle_has_center = false;
+		}
+    break;
 }
 
 // in the objects keyword i want to have lines or circles or other stuff
-void draw(AppState &app, uint32_t *buffer) {
+void draw(AppState &app, Objects &objects) {
 	auto t1 = std::chrono::high_resolution_clock::now();
 	void *pixels;
 	int pitch;
   if (SDL_LockTexture(app.window_texture, NULL, &pixels, &pitch)) {
-
-		// std::memset(pixels, 0xFF, 4 * app.w_pixels * app.h_pixels);
-    // fast_fill_neon((uint32_t*)pixels, 0xFFFFFF00, app.w_pixels * app.h_pixels);
-
-		auto in_t1 = std::chrono::high_resolution_clock::now();
 		std::fill_n((uint32_t*)pixels, app.w_pixels * app.h_pixels, 0xFF00FFFF);
-		auto in_t2 = std::chrono::high_resolution_clock::now();
-		std::chrono::duration<double, std::milli> 
-			dt_ms_in = in_t2 - in_t1;
-		std::cout << "dt_ins: " << dt_ms_in << std::endl;
-
-		// for (uint32_t i = 0;	i < app.w_pixels * app.h_pixels; i++) {
-		// 	((uint32_t *)pixels)[i] = 0XFFFFFF00;
-		// }	
-		// std::memcpy(pixels, buffer, 4 * app.w_pixels * app.h_pixels);
 		SDL_UnlockTexture(app.window_texture);
 	}
 
